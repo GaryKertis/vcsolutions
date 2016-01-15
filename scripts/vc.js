a5ef645592fb5e5959a962aeb5398037bdfb283d
Gary Kertis <gary.kertis@pubmatic.com>
2016-01-15 11:30:48 -0500

    Add scroll functionality.

--- scripts/vc.js
+++ scripts/vc.js
@@ -1,4 +1,4 @@
-var vcApp = function(){
+var vcApp = function() {
     var bg;
     var interval;
     var opacity = 0;
@@ -8,7 +8,8 @@ var vcApp = function(){
 
     return {
         showBg: showBg,
-        hideBg: hideBg
+        hideBg: hideBg,
+        imgClick: imgClick
     }
 
     function showBg() {
@@ -33,5 +34,32 @@ var vcApp = function(){
                 clearInterval(interval);
             }
         }, 10)
+    }
+
+    function imgClick(target) {
+        var scrollContainer = target;
+        do { //find scroll container
+            scrollContainer = scrollContainer.parentNode;
+            if (!scrollContainer) return;
+            scrollContainer.scrollTop += 1;
+        } while (scrollContainer.scrollTop == 0);
+
+        var targetY = -100// -((window.innerHeight / 2) - (target.height / 2));
+        console.log(window.innerHeight, target.height, targetY);
+        do { //find the top of target relatively to the container
+            if (target == scrollContainer) break;
+            targetY += target.offsetTop;
+        } while (target = target.offsetParent);
+
+        scroll = function(c, a, b, i) {
+                i++;
+                if (i > 30) return;
+                c.scrollTop = a + (b - a) / 30 * i;
+                setTimeout(function() {
+                    scroll(c, a, b, i);
+                }, 5);
+            }
+            // start scrolling
+        scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
     }
 }()
